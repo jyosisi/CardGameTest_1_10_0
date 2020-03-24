74b280f808beb53e1848040be6a88e0a62a655d7
jyosisi <xu_zizi@163.com>
2020-03-24 11:34:13 +0800

    test

---
+++ src/settings.js
@@ -0,0 +1,39 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/Scene/helloworld.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/helloworld.fire",
+            uuid: "2dL3kvpAxJu6GJ7RdqJG5J"
+        }
+    ],
+    packedAssets: {
+        "010cf8dd1": [
+            "02delMVqdBD70a/HSD99FK",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "e8Ueib+qJEhL6mXAHdnwbi"
+        ],
+        "08317b1e5": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "2dL3kvpAxJu6GJ7RdqJG5J",
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
